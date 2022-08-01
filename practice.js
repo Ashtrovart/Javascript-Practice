976124ef938c28041d3670955bea601c639beb02
AshtrovArt <ashek.cift@gmail.com>
2022-08-01 21:12:39 +0600

    Array and push pop

--- practice.js
+++ practice.js
@@ -13,3 +13,21 @@ var WeightInKG= 75.6;
 // Boolean type variable 
 var married= true;
 var haveKids= false;
+
+// Array [writting a list of items under a variable name]
+var tableItems =['Bottle', 'Marker', 'Pen', 'Diary', 'Mobile Stand']
+
+// get items 
+var Item3 = tableItems[3];
+
+// findout index number of any item 
+var DiaryIndex= tableItems.indexOf('Diary')
+console.log(DiaryIndex)
+
+// set items (change the value of previously named item from the list) 
+tableItems[4]= 'Pen Stand'
+console.log(tableItems)
+// Push = Add item in end of the List 
+// Unshift = Add item in the Begining
+// Pop = Remove item from the End of the list
+// Shift = Remove item from the begining
