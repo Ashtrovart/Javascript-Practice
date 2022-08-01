6c83677724fe9e94c10270ec0c39f841c832e3ba
AshtrovArt <ashek.cift@gmail.com>
2022-08-01 20:52:00 +0600

    variable type

--- practice.js
+++ practice.js
@@ -1,4 +1,15 @@
-var firstName= 'Ashek'
-var lastName= 'Rabbani'
+// String type Variable
+var firstName= 'Abdul';
+var lastName= 'Kareem';
 var fullName= [firstName+" "+lastName]
 console.log(fullName);
+
+// Number type variable (Integer)
+var Age= 31;
+
+// Number type variable (float) 
+var WeightInKG= 75.6;
+
+// Boolean type variable 
+var married= true;
+var haveKids= false;
