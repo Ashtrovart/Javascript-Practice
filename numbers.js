ed0536cc12bc6fea4a227b7dfcb6d453bfd9fed4
AshtrovArt <ashek.cift@gmail.com>
2022-08-02 12:29:14 +0600

    numbers

---
+++ numbers.js
@@ -0,0 +1,9 @@
+// target = Display numbers 1-10
+var numbers = 1;
+// while(condition){output} 
+while (numbers <=10){
+    console.log(numbers)
+    // numbers= numbers+1; 
+    // numbers+=1; 
+    numbers++;
+}
